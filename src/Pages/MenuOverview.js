e7f9f79212237f5235bca7eb2dbb175679de4233
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-06-13 11:46:16 +0200

    Overview works with multiple tokens

--- src/Pages/MenuOverview.js
+++ src/Pages/MenuOverview.js
@@ -5,18 +5,24 @@ import MenuCard from "../Components/MenuCard";
 
 const MenuOverview = () => {
     const [ isMounted, setIsMounted ] = React.useState(false);
+    const [ menus, setMenus ] = React.useState([]);
+    const tempMenus = [];
 
-    const menus = [];
     const menuTokens = [
-        "sjhsudugdudhu"
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJNZW51SUQiOjE1LCJPd25lciI6ImZkMmIxYmNkMTBiNzdjZjAxZmYwZjk1NjQ5MmM1M2UwN2JmOGEyMjUwZTA1MWRlODdiN2FmNjRkNWVmMzNiM2MifQ._cilftkNsW4xxIdaJGY80Zw_0f67i3_rDgKcsJ2gOGg",
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJNZW51SUQiOjIwLCJPd25lciI6ImZkMmIxYmNkMTBiNzdjZjAxZmYwZjk1NjQ5MmM1M2UwN2JmOGEyMjUwZTA1MWRlODdiN2FmNjRkNWVmMzNiM2MifQ.cNMdvcwa4ICspzjxW2yC-LFJb6iaE-IAWeqbc_9AYcA"
     ];
 
     React.useEffect(() => {
-        !isMounted &&
+        !isMounted && 
             menuTokens.forEach((token, index) => {
                 GetMenu(token).then(json => {
-                    menus.push(json);
-                    index === menuTokens.length && setIsMounted(true);
+                    json.token = token;
+                    tempMenus.push(json);
+                    if (tempMenus.length === menuTokens.length) {
+                        setMenus(tempMenus);
+                        setIsMounted(true);
+                    }
                 });
             });
     });
@@ -24,16 +30,16 @@ const MenuOverview = () => {
     return (
         <Container className="mt-4">
             <Row>
-                <h1 style={{"text-align":"center"}}>Menu Overview</h1>
+                <h1 style={{textAlign:"center"}}>Menu Overview</h1>
             </Row>
             
             <Row className="row-cols-1 row-cols-md-3 g-4 mb-3">
                 { isMounted && 
-                    menus.map((menuItem, index) => (
+                    menus.map(menuItem => (
                         <MenuCard
                             key={ menuItem.title }
                             menu={ menuItem }
-                            href={"menudetail/" + menuTokens[index] } />
+                            href={"menudetail/" + menuItem.token } />
                     ))
                 }
             </Row>
